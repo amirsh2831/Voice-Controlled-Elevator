f91ec5cd08ad85af8d83f05c993f44ae6ca6fa35
amirsh2831 <amirsh2831@gmail.com>
2025-05-16 18:23:57 +0330

    average, energy & zcr

---
+++ elv.py
@@ -0,0 +1,67 @@
+import numpy as np
+import scipy.io.wavfile as wav
+import matplotlib.pyplot as plt
+
+sample_rate, data = wav.read('voice-2.wav')
+frame_size = 1024
+num_frames = len(data) // frame_size
+
+averages = []
+energies = []
+zero_crossings = []
+
+
+# stereo to mono
+if len(data.shape) == 2:
+    print("data.shape 2")
+    data = np.mean(data, axis=1)
+
+
+
+for i in range(num_frames):
+    frame = data[i * frame_size : (i + 1) * frame_size]
+    avg = np.mean(frame)
+    energy = np.sum(frame ** 2)
+
+    # --------------------
+    signs = np.sign(frame)
+    notzero = signs != 0
+    zero_filter = signs[notzero]
+
+    if len(zero_filter) > 1:
+        zcr = np.mean(np.abs(np.diff(zero_filter)))
+    else:
+        zcr = 0
+    # --------------------
+
+    averages.append(avg)
+    energies.append(energy)
+    zero_crossings.append(zcr)
+
+
+
+frames = np.arange(num_frames)
+
+
+
+fig, axs = plt.subplots(3, 1, figsize=(12, 10), sharex=True)
+
+axs[0].plot(frames, averages, label='Average', color='blue')
+axs[0].set_ylabel('Average Amplitude')
+axs[0].legend()
+axs[0].grid(True)
+
+axs[1].plot(frames, energies, label='Energy', color='green')
+axs[1].set_ylabel('Energy')
+axs[1].legend()
+axs[1].grid(True)
+
+axs[2].plot(frames, zero_crossings, label='Zero Crossings', color='red')
+axs[2].set_xlabel('Frame Index')
+axs[2].set_ylabel('Zero Crossings')
+axs[2].legend()
+axs[2].grid(True)
+
+plt.suptitle('elevator mk II')
+plt.tight_layout(rect=[0, 0.03, 1, 0.95])
+plt.show()
